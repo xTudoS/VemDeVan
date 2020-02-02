7263e0dff32ed0c98238c3a47735e6da558afce1
Alexandre <alexandrerjp13@gmail.com>
2020-02-02 11:51:51 -0300

    update painel de controle

--- control_admin/painel.py
+++ control_admin/painel.py
@@ -5,7 +5,9 @@ class Painel(tk.Frame):
     def __init__(self, master, *args, **kw):
         tk.Frame.__init__(self, master)
 
-        self.title('Painel de Controle - Vem de Van')
+        self.master = master
+
+        self.master.title('Painel de Controle - Vem de Van')
         self['bg'] = 'black'
 
         button = tk.Button(text='Close', command=lambda : exit())
